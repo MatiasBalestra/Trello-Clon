c381818d8933ca1d776bd58699c3146a0a9680be
Matias Balestra <mbalestra@applica.dev>
2023-07-13 11:56:57 -0300

    stylos dinamicos al componente color

--- src/app/components/btn/btn.component.ts
+++ src/app/components/btn/btn.component.ts
@@ -8,11 +8,22 @@ export class BtnComponent implements OnInit {
 
 
   @Input() typeBtn: 'button' | 'reset' | 'submit' = 'button';
-
+  @Input() color = 'success';
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  get colors() {
+    const colorsList: { [key: string]: string } = {
+      success: 'bg-success-700 hover:bg-success-800 focus:ring-success-300',
+      danger: 'bg-red-700 hover:bg-red-800 focus:ring-red-300',
+      primary: 'bg-primary-700 hover:bg-primary-800 focus:ring-primary-300',
+    };
+    return colorsList[this.color];
+  }
 }
+
+
+
